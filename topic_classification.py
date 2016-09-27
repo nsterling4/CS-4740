6d7814a2052ceafd651913bc609ca4093e57bd07
Noah Sterling <nsterling4@Noah-Sterlings-MacBook-Pro.local>
2016-09-26 21:58:36 -0400

    assignment 2 files

--- topic_classification.py
+++
@@ -1,9 +0,0 @@
-import csv
-r = csv.reader(open("perplexity_final.csv", "r"))
-c = csv.writer(open("perplexity_w_class.csv", "wb"),delimiter=',')
-next(r, None)  # skip the headers
-topics = ["motorcycles","medicine","atheism","autos","graphics","religion","space"]
-for row in r:
-	nums = [row[1],row[2],row[3],row[4],row[5],row[6],row[7]]
-	min_perplex = nums.index(min(nums))
-	c.writerow([row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],topics[min_perplex]])
